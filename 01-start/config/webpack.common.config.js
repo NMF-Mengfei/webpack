375eac1500dc887503d57abe69b0d570934329fe
niumengfei <870424431@qq.com>
2020-08-19 11:19:23 +0800

    webpack配置3/4‘
    git commit -mwebpack配置3/4‘

--- 01-start/config/webpack.common.config.js
+++ 01-start/config/webpack.common.config.js
@@ -2,7 +2,7 @@
  * @Author: niumengfei
  * @Date: 2020-08-17 16:07:29
  * @LastEditors: niumengfei
- * @LastEditTime: 2020-08-18 14:42:34
+ * @LastEditTime: 2020-08-19 11:18:33
  */
 // console.log('webpack-common...')
 const path = require('path');
@@ -17,12 +17,34 @@ module.exports = {
         filename: 'js/bundle.js',
         path: path.resolve(__dirname,'../dist')
     },
+    devtool: 'cheap-module-eval-source-map',
     module: {
         rules: [
           {
             test: /\.(js|jsx)$/,
             use: 'babel-loader',
             exclude: /node_modules/,
+          },
+          {
+            test: /\.(jpg|png|gif)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+                limit: 8192,
+              },
+            },
+          },
+          {
+            test: /\.(eot|ttf|svg|woff|woff2)$/,
+            use: {
+              loader: 'file-loader',
+              options: {
+                name: '[name]_[hash].[ext]',
+                outputPath: 'font/'
+              }
+            }
           }
         ]
     }
--- 01-start/src/App.js
+++ 01-start/src/App.js
@@ -2,7 +2,7 @@
  * @Author: niumengfei
  * @Date: 2020-08-17 16:13:31
  * @LastEditors: niumengfei
- * @LastEditTime: 2020-08-18 17:05:07
+ * @LastEditTime: 2020-08-19 10:40:14
  */
 // var root = document.getElementById('root');
 // root.innerHTML = '我是夜语清梦'
@@ -11,11 +11,12 @@
 import React from 'react';
 // import './App.css';
 import './App.less';
-
+import bg from './images/bg1.jpg'
 function APP(){
     return(
         <div className='APP'>
-            <p>夜语清梦哈哈12</p>
+            <p>夜语清梦哈哈12<i className='iconfont'>&#xe622;</i></p>
+            <img className="background" src={bg} alt=""/>
         </div>
     )
 }
